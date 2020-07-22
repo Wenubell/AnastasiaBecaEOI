8382838b97a21e1c15db69d811e5cc060f567d10
Anastasia <ac133@alu.ua.es>
2020-07-22 13:24:57 +0200

    test

--- UserREST/src/test/java/edu/es/eoi/user/UserRestApplicationJPAImplTests2.java
+++ UserREST/src/test/java/edu/es/eoi/user/UserRestApplicationJPAImplTests2.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.es.eoi.user.entity.User;
 import edu.es.eoi.user.repository.MyRepository;
+import edu.es.eoi.user.service.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,11 +22,26 @@ class UserRestApplicationJPAImplTests2 {
 	@Autowired
 	@Qualifier("JPA")
 	MyRepository<User> repository;
+	
+	@Autowired
+	UserService service;
 
 	@Test
-	void testFindUserById() {		
-		assertEquals("JJ",repository.findById(1).getNombre());		
+	void testFindUserById() {
+		assertEquals("JJ", repository.findById(1).getNombre());
 	}
 
+	@Test
+	void testCreateUser() {
+
+		User user = new User();
+		user.setNombre("prueba");
+		repository.create(user);	
+	}
+
+	@Test
+	void testDeleteUser() throws Exception {
+		service.deleteById(2);
+	}
 
 }
