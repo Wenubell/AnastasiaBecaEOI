293215439d69b18ccbc5ee8bed06246c3d5fd39b
Anastasia <ac133@alu.ua.es>
2020-07-24 19:00:21 +0200

    Primer Commit
    Proyecto final bootcamp java EOI

---
+++ mundobancario/src/main/java/es/eoi/mundobancario/MundobancarioApplication.java
@@ -0,0 +1,13 @@
+package es.eoi.mundobancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundobancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundobancarioApplication.class, args);
+	}
+
+}
