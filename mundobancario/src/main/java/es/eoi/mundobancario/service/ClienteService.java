7168c624cad0ac5bf04234ff4b01cfe571ac3162
Anastasia <ac133@alu.ua.es>
2020-07-31 19:57:46 +0200

    commit

--- mundobancario/src/main/java/es/eoi/mundobancario/service/ClienteService.java
+++ mundobancario/src/main/java/es/eoi/mundobancario/service/ClienteService.java
@@ -1,22 +1,28 @@
 package es.eoi.mundobancario.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import es.eoi.mundobancario.dto.ClienteDto;
-
+import es.eoi.mundobancario.dto.ClienteSimpleDto;
+import es.eoi.mundobancario.dto.CuentaDto;
 
 public interface ClienteService {
-	
-	
-	public ClienteDto findClienteById(Integer id);
-	
-	public List<ClienteDto> findAll();
 
-	public ClienteDto loginUsuario(String usuario, String pass);
+	public ClienteSimpleDto findClienteById(Integer id);
+
+	public List<ClienteSimpleDto> findAll();
+
+	public ClienteSimpleDto loginUsuario(String usuario, String pass);
+
+	public List<CuentaDto> findCuentasByIdUsuario(Integer id);
 
 	public void crearUsuario(ClienteDto cliente);
 
 	public void updateCliente(ClienteDto cliente);
+	
+	//-----------------------------------------
 
-	public Object findClienteCompleto();
+	public HashMap<ClienteSimpleDto, List<CuentaDto>> findClientesCompletos();
+
 }
--- mundobancario/src/main/java/es/eoi/mundobancario/service/ClienteServiceImpl.java
+++ mundobancario/src/main/java/es/eoi/mundobancario/service/ClienteServiceImpl.java
@@ -1,75 +1,109 @@
+
 package es.eoi.mundobancario.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.eoi.mundobancario.dto.ClienteDto;
+import es.eoi.mundobancario.dto.ClienteSimpleDto;
+import es.eoi.mundobancario.dto.CuentaDto;
 import es.eoi.mundobancario.entity.Cliente;
+import es.eoi.mundobancario.entity.Cuenta;
 import es.eoi.mundobancario.repository.ClienteRepository;
+import es.eoi.mundobancario.util.Util;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
-	
+
 	@Autowired
 	ClienteRepository repository;
-	
-	public ClienteDto findClienteById(Integer id) {
-		return convertToDto(repository.findById(id).get());
+
+	public ClienteSimpleDto findClienteById(Integer id) {
+		return Util.convertToClienteSimpleDto(repository.findById(id).get());
 	}
-	
-	public List<ClienteDto> findAll() {
+
+	public List<ClienteSimpleDto> findAll() {
 		List<Cliente> clientes = new ArrayList<Cliente>();
 		clientes.addAll(repository.findAll());
-		List<ClienteDto> clientesDto = new ArrayList<ClienteDto>();
+		List<ClienteSimpleDto> clientesDto = new ArrayList<ClienteSimpleDto>();
 		for (Cliente cliente : clientes) {
-			clientesDto.add(convertToDto(cliente));
+			clientesDto.add(Util.convertToClienteSimpleDto(cliente));
 		}
 		return clientesDto;
 	}
-	
-	public ClienteDto loginUsuario(String usuario, String pass) {
+
+	public ClienteSimpleDto loginUsuario(String usuario, String pass) {
 		Cliente cli = repository.findClienteByUsuario(usuario);
-		if(cli.getPass().equals(pass)) {
-			return convertToDto(cli);
+		if (cli.getPass().equals(pass)) {
+			return Util.convertToClienteSimpleDto(cli);
 		}
 		return null;
 	}
-	
-	public void crearUsuario(ClienteDto dto) {
-		repository.save(convertToCliente(dto));
+
+	public List<CuentaDto> findCuentasByIdUsuario(Integer id) {
+
+		List<Cuenta> cuentas = repository.findById(id).get().getCuantas();
+		List<CuentaDto> cuentasDto = new ArrayList<CuentaDto>();
+
+		for (Cuenta cuenta : cuentas) {
+			cuentasDto.add(Util.convertToCuentaDto(cuenta));
+		}
+
+		return cuentasDto;
 	}
-	
+
+	public void crearUsuario(ClienteDto dto) {
+		repository.save(Util.convertToCliente(dto));
+	}
+
 	public void updateCliente(ClienteDto dto) {
 		Cliente cli = repository.findClienteByUsuario(dto.getUsuario());
-		if(cli.getPass().equals(dto.getPass())) {
+		if (cli.getPass().equals(dto.getPass())) {
 			cli.setEmail(dto.getEmail());
 			repository.save(cli);
 		}
 	}
-	
-	public Object findClienteCompleto() {
-		return null;
-	}
-	
-	private ClienteDto convertToDto(Cliente cliente) {
-		ClienteDto dto = new ClienteDto();
-		dto.setEmail(cliente.getEmail());
-		dto.setNombre(cliente.getNombre());
-		dto.setPass(cliente.getPass());
-		dto.setUsuario(cliente.getUsuario());
-		return dto;
-	}
-	
-	private Cliente convertToCliente(ClienteDto dto) {
-		Cliente cliente = new Cliente();
-		cliente.setEmail(dto.getEmail());
-		cliente.setNombre(dto.getNombre());
-		cliente.setPass(dto.getPass());
-		cliente.setUsuario(dto.getUsuario());
-		return cliente;
+
+	public HashMap<ClienteSimpleDto, List<CuentaDto>> findClientesCompletos() {
+
+		List<Cliente> clientes = repository.findAll();
+		List<CuentaDto> cuentasDto = new ArrayList<CuentaDto>();
+		HashMap<ClienteSimpleDto, List<CuentaDto>> todos = new HashMap<ClienteSimpleDto, List<CuentaDto>>();
+
+		for (Cliente cliente : clientes) {
+
+			for (Cuenta cuenta : cliente.getCuantas()) {
+				cuentasDto.add(Util.convertToCuentaDto(cuenta));
+			}
+
+			todos.put(Util.convertToClienteSimpleDto(cliente), cuentasDto);
+			cuentasDto.clear();
+		}
+
+		return todos;
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
